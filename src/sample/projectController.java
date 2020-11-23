84714d0e1be3f912e387d5be96665ea58fe35c60
Shaochen <shaohockey@gmail.com>
2020-11-22 23:09:43 -0800

    Login Window now functional

--- src/sample/projectController.java
+++ src/sample/projectController.java
@@ -1,13 +1,20 @@
 package sample;
 
+import controller.NGO;
+import database.DatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class projectController {
+
+    private DatabaseHandler databaseHandler = new DatabaseHandler();
+    private int attempts = 0;
 
     @FXML
     private TextField username;
@@ -23,7 +30,19 @@ public class projectController {
 
     @FXML
     void buttonPressed(ActionEvent event) {
-        System.out.println("TEST");
+        Stage loginWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        if (databaseHandler.login(username.getText(), password.getText())) {
+            System.out.println("LOGIN SUCCESSFUL");
+            loginStatus.setText("Connected!");
+        } else {
+            System.out.println("LOGIN FAILED");
+            loginStatus.setText("Login Failed");
+            attempts++;
+
+            if (attempts == 3) {
+                loginWindow.close();
+            }
+        }
     }
 
 }
